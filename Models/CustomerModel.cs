bd5be089cab167e9a102418d8d02aecfae748f91
Ziad Khaled <ziadkh99@hotmail.com>
2023-05-03 00:51:39 +0300

    Accociating File Uploads with a Form

---
+++ Models/CustomerModel.cs
@@ -0,0 +1,11 @@
+namespace UploadFilesApp.Models;
+
+
+public class CustomerModel 
+{
+    public int Id { get; set; }
+    public string UserName { get; set; } = "ziadkh";
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? FileName { get; set; }
+}
